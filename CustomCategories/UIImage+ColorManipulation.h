68d0796effcd3af81a6e2da39c4898d0cb1676c8
Blake Nazario <blake.nazario@yahoo.com>
2016-06-08 17:29:44 -0400

    Finished documentation-formatted comments for UIImage+ColorManipulation

--- CustomCategories/UIImage+ColorManipulation.h
+++ CustomCategories/UIImage+ColorManipulation.h
@@ -25,17 +25,48 @@
 **/
 + (UIImage *)setBackgroundImageByColor:(UIColor *)backgroundColor withFrame:(CGRect )rect;
 
-// Replace every pixel in an image, that is equal to a single specified color, to another color
-// that exists within a specified tolerance
-+ (UIImage *)replaceColor:(UIColor*)color inImage:(UIImage*)image withTolerance:(float)tolerance;
+/**
+ Replace every pixel in an image, that is equal to a single specified color, to another color
+ that exists within a specified tolerance
+ 
+ @param color The color that will be replaced.
+ @param image The image that will be modified.
+ @param tolerance The tolerance of colors differing from the color that is being replaced.
+ 
+ @warning `color` Color must not be `nil`.
+ @warning `image` Image must not be `nil`.
+ @warning `tolerance` Tolerance must not be `nil`.
+ **/
+ + (UIImage *)replaceColor:(UIColor*)color inImage:(UIImage*)image withTolerance:(float)tolerance;
 
-// Change every white pixel in an image to a transparent pixel
+/**
+ Change every white pixel in an image to a transparent pixel
+ 
+ @param image The image that will be modified.
+ 
+ @warning `image` Image must not be `nil`.
+ **/
 + (UIImage *)changeWhiteColorTransparent: (UIImage *)image;
 
-// Change every pixel in an image, that is equal to a color specified in an array, to a transparent pixel
+/**
+ Change every pixel in an image, that is equal to a color specified in an array,
+ to a transparent pixel
+ 
+ @param array An array of colors that that will be changed to transparent.
+ @param image The image that will be modified.
+ 
+ @warning `array` Array must not be `nil`.
+ @warning `image` Image must not be `nil`.
+ **/
 + (UIImage *)changeColorTo:(NSMutableArray*)array Transparent:(UIImage *)image;
 
-// Render negative of image
+/**
+ Render negative of image
+ 
+ @param sourceImage The image that will be modified.
+ 
+ @warning `sourceImage` Source Image must not be `nil`.
+**/
 + (UIImage *)negativeImageWithImage:(UIImage*)sourceImage;
 
 @end
